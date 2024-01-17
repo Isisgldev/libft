dfc17e69610c393d8c98064fe2ed014ceb3fc4f4
Isis Gómez López <43893808+Isisgldev@users.noreply.github.com>
2024-01-17 21:29:20 +0100

    Add files via upload

---
+++ libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isgomez- <isgomez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 11:18:47 by isgomez-          #+#    #+#             */
+/*   Updated: 2024/01/17 18:24:39 by isgomez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdio.h>
+
+int ft_isalpha(int i);
+int ft_isdigit(int i);
+int ft_isalnum(int i);
+int ft_isprint(int i);
+int ft_isascii(int i);
+int ft_toupper(int i);
+int ft_tolower(int i);
+
+#endif
